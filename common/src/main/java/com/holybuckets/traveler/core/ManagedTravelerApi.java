2f13cf7f8658da196f2e244d746fec61fcdefab3
jhw2167 <jhw2167@columbia.edu>
2026-03-06 15:41:22 -0600

    feat: Add getManagedTraveler method with null safety checks

--- common/src/main/java/com/holybuckets/traveler/core/ManagedTravelerApi.java
+++ common/src/main/java/com/holybuckets/traveler/core/ManagedTravelerApi.java
@@ -1,6 +1,7 @@
 package com.holybuckets.traveler.core;
 
 import com.holybuckets.foundation.HBUtil;
+import com.holybuckets.foundation.player.ManagedPlayer;
 import com.holybuckets.traveler.LoggerProject;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
@@ -15,9 +16,28 @@ public class ManagedTravelerApi {
         if(player instanceof ServerPlayer) {
             String id = HBUtil.PlayerUtil.getId(player);
             ManagedTraveler mp = TRAVELERS.get(id);
+            if(mp == null) {
+                LoggerProject.logError("034001", "No ManagedTraveler found for player with id " + id);
+                return;
+            }
             mp.clearSoulboundSlots();
         } else {
             LoggerProject.logError("034000", "ManagedTravelerApi::clearSoulboundSlots should only be called on the server side.");
+        }
+    }
+
+    static ManagedTraveler getManagedTraveler(Player player) {
+        if(player instanceof ServerPlayer) {
+            String id = HBUtil.PlayerUtil.getId(player);
+            ManagedTraveler mp = TRAVELERS.get(id);
+            if(mp == null) {
+                LoggerProject.logError("034002", "No ManagedTraveler found for player with id " + id);
+                return null;
+            }
+            return mp;
+        } else {
+            LoggerProject.logError("034000", "ManagedTravelerApi::getManagedPlayer should only be called on the server side.");
+            return null;
         }
     }
 
